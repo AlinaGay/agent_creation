a0a623f00e2a753d63439f6edde1079a050e8d49
AlinaGay <gayfullina.a@gmail.com>
2025-10-04 23:43:58 +0200

    Add cheking LLMs

--- my_code/second.py
+++ my_code/second.py
@@ -4,3 +4,65 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from anthropic import Anthropic
 from IPython.display import Markdown, display
+
+
+load_dotenv(override=True)
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+anthropic_api_key = os.getenv('ANTHROPIC_API_KEY')
+google_api_key = os.getenv('GOOGLE_API_KEY')
+deepseek_api_key = os.getenv('DEEPSEEK_API_KEY')
+groq_api_key = os.getenv('GROQ_API_KEY')
+
+if openai_api_key:
+    print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
+else:
+    print("OpenAI API Key not set")
+    
+if anthropic_api_key:
+    print(f"Anthropic API Key exists and begins {anthropic_api_key[:7]}")
+else:
+    print("Anthropic API Key not set (and this is optional)")
+
+if google_api_key:
+    print(f"Google API Key exists and begins {google_api_key[:2]}")
+else:
+    print("Google API Key not set (and this is optional)")
+
+if deepseek_api_key:
+    print(f"DeepSeek API Key exists and begins {deepseek_api_key[:3]}")
+else:
+    print("DeepSeek API Key not set (and this is optional)")
+
+if groq_api_key:
+    print(f"Groq API Key exists and begins {groq_api_key[:4]}")
+else:
+    print("Groq API Key not set (and this is optional)")
+
+
+request = "Please come up with a challenging, nuanced question that I can ask a number of LLMs to evaluate their intelligence. "
+request += "Answer only with the question, no explanation."
+messages = [{"role": "user", "content": request}]
+
+messages
+
+openai = OpenAI()
+response = openai.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=messages,
+)
+question = response.choices[0].message.content
+print(question)
+
+competitors = []
+answers = []
+messages = [{"role": "user", "content": question}]
+
+model_name = "gpt-4o-mini"
+
+response = openai.chat.completions.create(model=model_name, messages=messages)
+answer = response.choices[0].message.content
+
+display(Markdown(answer))
+competitors.append(model_name)
+answers.append(answer)
