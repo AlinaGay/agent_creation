6517bc940486db01e39935c630a0b7b6b3a2bac7
AlinaGay <gayfullina.a@gmail.com>
2025-10-08 22:30:30 +0200

    Add changes

--- my_code/2_checking_different_LLMs.py
+++ my_code/2_checking_different_LLMs.py
@@ -18,7 +18,7 @@ if openai_api_key:
     print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
 else:
     print("OpenAI API Key not set")
-    
+
 if anthropic_api_key:
     print(f"Anthropic API Key exists and begins {anthropic_api_key[:7]}")
 else:
@@ -58,6 +58,7 @@ competitors = []
 answers = []
 messages = [{"role": "user", "content": question}]
 
+# gpt-4o-mini
 model_name = "gpt-4o-mini"
 
 response = openai.chat.completions.create(model=model_name, messages=messages)
@@ -66,3 +67,66 @@ answer = response.choices[0].message.content
 display(Markdown(answer))
 competitors.append(model_name)
 answers.append(answer)
+
+# gemini-2.0-flash
+
+gemini = OpenAI(api_key=google_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
+model_name = "gemini-2.0-flash"
+
+response = gemini.chat.completions.create(model=model_name, messages=messages)
+answer = response.choices[0].message.content
+
+display(Markdown(answer))
+competitors.append(model_name)
+answers.append(answer)
+
+
+# check
+print(competitors)
+print(answers)
+
+# comparison
+for competitor, answer in zip(competitors, answers):
+    print(f"Competitor: {competitor}\n\n{answer}")
+
+together = ""
+for index, answer in enumerate(answers):
+    together += f"# Response from competitor {index+1}\n\n"
+    together += answer + "\n\n"
+
+print(together)
+
+judge = f"""You are judging a competition between {len(competitors)} competitors.
+Each model has been given this question:
+
+{question}
+
+Your job is to evaluate each response for clarity and strength of argument, and rank them in order of best to worst.
+Respond with JSON, and only JSON, with the following format:
+{{"results": ["best competitor number", "second best competitor number", "third best competitor number", ...]}}
+
+Here are the responses from each competitor:
+
+{together}
+
+Now respond with the JSON with the ranked order of the competitors, nothing else. Do not include markdown formatting or code blocks."""
+
+print(judge)
+
+judge_messages = [{"role": "user", "content": judge}]
+
+# Judgement time!
+
+openai = OpenAI()
+response = openai.chat.completions.create(
+    model="o3-mini",
+    messages=judge_messages,
+)
+results = response.choices[0].message.content
+print(results)
+
+results_dict = json.loads(results)
+ranks = results_dict["results"]
+for index, result in enumerate(ranks):
+    competitor = competitors[int(result)-1]
+    print(f"Rank {index+1}: {competitor}")
