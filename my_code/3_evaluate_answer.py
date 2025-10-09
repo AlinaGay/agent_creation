7fed36824ee9b1f5374572c7c70354f1a9e6b5da
AlinaGay <gayfullina.a@gmail.com>
2025-10-09 23:54:40 +0200

    Add new chat function

--- my_code/3_evaluate_answer.py
+++ my_code/3_evaluate_answer.py
@@ -37,7 +37,7 @@ system_prompt += f"With this context, please chat with the user, always staying 
 print('system_prompt: ', system_prompt)
 
 
-def chat(message, history):
+def chat_1(message, history):
     messages = [{"role": "system", "content": system_prompt}] + history + [{"role": "user", "content": message}]
     response = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
     return response.choices[0].message.content
@@ -57,9 +57,61 @@ The Agent has been provided with context on {name} in the form of their summary 
 evaluator_system_prompt += f"\n\n## Summary:\n{summary}\n\n## LinkedIn Profile:\n{linkedin}\n\n"
 evaluator_system_prompt += f"With this context, please evaluate the latest response, replying with whether the response is acceptable and your feedback."
 
+
 def evaluator_user_prompt(reply, message, history):
     user_prompt = f"Here's the conversation between the User and the Agent: \n\n{history}\n\n"
     user_prompt += f"Here's the latest message from the User: \n\n{message}\n\n"
     user_prompt += f"Here's the latest response from the Agent: \n\n{reply}\n\n"
     user_prompt += "Please evaluate the response, replying with whether it is acceptable and your feedback."
     return user_prompt
+
+gemini = OpenAI(
+    api_key=os.getenv("GOOGLE_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+def evaluate(reply, message, history) -> Evaluation:
+    messages = [{"role": "system", "content": evaluator_system_prompt}] + [{"role": "user", "content": evaluator_user_prompt(reply, message, history)}]
+    response = gemini.beta.chat.completions.parse(model="gemini-2.0-flash", messages=messages, response_format=Evaluation)
+    return response.choices[0].message.parsed
+
+messages = [{"role": "system", "content": system_prompt}] + [{"role": "user", "content": "do you hold a patent?"}]
+response = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
+reply = response.choices[0].message.content
+
+reply
+
+evaluate(reply, "do you hold a patent?", messages[:1])
+
+
+def rerun(reply, message, history, feedback):
+    updated_system_prompt = system_prompt + "\n\n## Previous answer rejected\nYou just tried to reply, but the quality control rejected your reply\n"
+    updated_system_prompt += f"## Your attempted answer:\n{reply}\n\n"
+    updated_system_prompt += f"## Reason for rejection:\n{feedback}\n\n"
+    messages = [{"role": "system", "content": updated_system_prompt}] + history + [{"role": "user", "content": message}]
+    response = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
+    return response.choices[0].message.content
+
+
+def chat_2(message, history):
+    if "patient" in message:
+        system = system_prompt + "\n\nEverything in your reply needs to be in pig latin - \
+              it is mandatory that you respond only and entirely in pig latin"
+    else:
+        system = system_prompt
+    messages = [{"role": "system", "content": system}] + history + [{"role": "user", "content": message}]
+    response = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
+    reply = response.choices[0].message.content
+
+    evaluation = evaluate(reply, message, history)
+
+    if evaluation.is_acceptable:
+        print("Passed evaluation - returning reply")
+    else:
+        print("Failed evaluation - retrying")
+        print(evaluation.feedback)
+        reply = rerun(reply, message, history, evaluation.feedback)
+    return reply
+
+
+gradio.ChatInterface(chat_2, type="messages").launch()
