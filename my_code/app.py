ef70f73a3e9f46c8231336dcbff861434a2b3d32
AlinaGay <gayfullina.a@gmail.com>
2025-10-20 19:12:16 +0200

    Add push function

--- my_code/app.py
+++ my_code/app.py
@@ -8,3 +8,14 @@ import gradio as gr
 
 
 load_dotenv(override=True)
+
+def push(text):
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": os.getenv("PUSHOVER_TOKEN"),
+            "user": os.getenv("PUSHOVER_USER"),
+            "message": text,
+        }
+    )
+
