5cb89e2fb61dfd02a82c3372295a5e759bbe36e4
AlinaGay <gayfullina.a@gmail.com>
2025-11-01 23:24:42 +0100

    Add trace

--- 2_openai/2_lab.py
+++ 2_openai/2_lab.py
@@ -7,3 +7,52 @@ import os
 import asyncio
 
 load_dotenv(override=True)
+RESEND_API_KEY = os.getenv('RESEND_API_KEY')
+
+instructions1 = "You are a sales agent working for ComplAI, \
+a company that provides a SaaS tool for ensuring SOC2 compliance and preparing for audits, powered by AI. \
+You write professional, serious cold emails."
+
+instructions2 = "You are a humorous, engaging sales agent working for ComplAI, \
+a company that provides a SaaS tool for ensuring SOC2 compliance and preparing for audits, powered by AI. \
+You write witty, engaging cold emails that are likely to get a response."
+
+instructions3 = "You are a busy sales agent working for ComplAI, \
+a company that provides a SaaS tool for ensuring SOC2 compliance and preparing for audits, powered by AI. \
+You write concise, to the point cold emails."
+
+sales_agent_1 = Agent(
+    name="Professional Sales Agent",
+    instructions=instructions1,
+    model="gpt-4o-mini"
+)
+
+sales_agent_2 = Agent(
+    name="Engaging Sales Agent",
+    instructions=instructions2,
+    model="gpt-4o-mini"
+)
+
+sales_agent_3 = Agent(
+    name="Busy Sales Agent",
+    instructions=instructions3,
+    model="gpt-4o-mini"
+)
+
+result = Runner.run_streamed(sales_agent_1, input="Write a cold sales email")
+async for event in result.stream_events():
+    if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+        print(event.data.delta, end="", flush=True)
+
+message = "Write a cold sales email"
+
+with trace("Parallel col emails"):
+    results = await asyncio.gather(
+        Runner.run(sales_agent_1, message),
+        Runner.run(sales_agent_2, message),
+        Runner.run(sales_agent_3, message),
+    )
+
+outputs = [result.final_output for result in results]
+for output in outputs:
+    print(output + "\n\n")
