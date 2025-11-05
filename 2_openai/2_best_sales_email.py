ba4b8b925cd2200426dee834a26529803397db77
AlinaGay <gayfullina.a@gmail.com>
2025-11-05 11:09:05 +0100

    Add function tool send_html_email

--- 2_openai/2_best_sales_email.py
+++ 2_openai/2_best_sales_email.py
@@ -130,3 +130,44 @@ message = "Send a cold sales email addressed to 'Dear CEO'"
 
 with trace("Sales manager"):
     result = await Runner.run(sales_manager, message)
+
+
+subject_instructions = "You can write a subject for a cold sales email. \
+You are given a message and you need to write a subject for an email that is likely to get a response."
+
+html_instructions = "You can convert a text email body to an HTML email body. \
+You are given a text email body which might have some markdown \
+and you need to convert it to an HTML email body with simple, clear, compelling layout and design."
+
+subject_writer = Agent(name="Email subject writer", instructions=subject_instructions, model="gpt-4o-mini")
+subject_tool = subject_writer.as_tool(tool_name="subject_writer", tool_description="Write a subject for a cold sales email")
+
+html_converter = Agent(name="HTML email body converter", instructions=html_instructions, model="gpt-4o-mini")
+html_tool = html_converter.as_tool(tool_name="html_converter", tool_description="Convert a text email body to an HTML email body")
+
+@function_tool
+def send_html_email(subject: str, html_body: str) -> Dict[str, str]:
+    """Send out an email with the given subject and HTML body to all sales prospects using Resend"""
+
+    from_email = "opolskaya.alina@yandex.ru"
+    to_email = "opolskaia.alina@gmail.com"
+
+    RESEND_API_KEY = os.environ.get("RESEND_API_KEY")
+    headers = {
+        "Authorization": f"Bearer {RESEND_API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "from": f"Alina <{from_email}>",
+        "to": [to_email],
+        "subject": subject,
+        "html": html_body,
+    }
+
+    response = requests.post("https://api.resend.com/emails", json=payload, headers=headers)
+
+    if response.status_code == 202:
+        return {"status": "success"}
+    else:
+        return {"status": "failure", "message": response.text}
