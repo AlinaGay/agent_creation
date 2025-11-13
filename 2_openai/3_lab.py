45ae7098a3a452846655b7d7da2a3b90a36d1118
AlinaGay <gayfullina.a@gmail.com>
2025-11-13 23:42:18 +0100

    Add changes

--- 2_openai/3_lab.py
+++ 2_openai/3_lab.py
@@ -139,3 +139,23 @@ message = "Send out a cold sales email addressed to Dear CEO from Alice"
 
 with trace("Automated SDR"):
     result = await Runner.run(sales_manager, message)
+
+
+class NameCheckOutput(BaseModel):
+    is_name_in_message: bool
+    name: str
+
+guardrail_agent = Agent(
+    name="Name check",
+    instructions="Check if the user is including someone's personal name in what they want you to do.",
+    output_type=NameCheckOutput,
+    model="gpt-4o-mini"
+)
+
+@input_guardrail
+async def guardrail_against_name(ctx, agent, message):
+    result = await Runner.run(guardrail_agent, message, context=ctx.context)
+    is_name_in_message = result.final_output.is_name_in_message
+    return GuardrailFunctionOutput(output_info={"found_name": result.final_output}, tripwire_triggered=is_name_in_message)
+
+
